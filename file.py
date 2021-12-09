eb53cf16dd4e58f0ef867cf26f3ee1478a66cde4
soubhagya1993 <mohapatra.soubhagya@gmail.com>
2021-12-09 12:38:03 +0530

    updated

---
+++ file.py
@@ -0,0 +1,11 @@
+import json
+from json2html import *
+
+
+with open ('input.json') as f:
+    d = json.load(f)
+    scanOutput =json2html.convert(json=d)
+    htmlReportFile = "outfile.html"
+    with open(htmlReportFile,'w') as htmlfile:
+        htmlfile.write(str(scanOutput))
+        print("successfully convrted")
